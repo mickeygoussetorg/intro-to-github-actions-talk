614e4f43cbc3b7243e3e6a5d017c2b9c6b68a2a1
Mickey Gousset <mickeygousset@github.com>
2021-09-30 15:29:00 -0500

    added failing test

--- mywebapp/tests/UnitTest1.cs
+++ mywebapp/tests/UnitTest1.cs
@@ -10,5 +10,22 @@ namespace tests
         {
 
         }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
