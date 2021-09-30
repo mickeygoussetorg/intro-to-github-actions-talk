d6d91e66fb2b978bd954571ac34b99cc689185ea
Mickey Gousset <mickeygousset@github.com>
2021-09-30 15:51:13 -0500

    test results test

--- mywebapp/tests/UnitTest1.cs
+++ mywebapp/tests/UnitTest1.cs
@@ -17,12 +17,6 @@ namespace tests
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
         int Add(int x, int y)
         {
             return x + y;
